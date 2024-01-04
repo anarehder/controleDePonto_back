09821bb5646e522372cbf1e7eb99deafc547d7e8
anarehder <anapaula.rehder@gmail.com>
2024-01-04 09:45:00 -0300

    feat: utils get request

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -2,4 +2,5 @@ export * from "./cannot-get-data-error";
 export * from "./conflict-error";
 export * from "./invalid-data-error";
 export * from "./not-found-error";
+export * from "./request-error";
 export * from "./unauthorized-error";
