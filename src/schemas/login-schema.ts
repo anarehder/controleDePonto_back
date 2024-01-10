fce9f3ebbddcc63733172e4c617b724d9c296c58
anarehder <anapaula.rehder@gmail.com>
2024-01-10 13:10:11 -0300

    fix: login schema

--- src/schemas/login-schema.ts
+++ src/schemas/login-schema.ts
@@ -2,6 +2,6 @@ import { LoginParams } from "@/protocols";
 import Joi from "joi";
 
 export const loginSchema = Joi.object<LoginParams>({
-    username: Joi.string().min(6).required(),
+    username: Joi.string().min(3).required(),
     password: Joi.string().min(6).required(),
 })
