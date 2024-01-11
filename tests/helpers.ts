2d849bd02a9ee2ca6b40982bb79619d704bb15e3
anarehder <anapaula.rehder@gmail.com>
2024-01-11 12:55:41 -0300

    feat: test helpers

---
+++ tests/helpers.ts
@@ -0,0 +1,21 @@
+import * as jwt from "jsonwebtoken";
+import { prisma } from "../src/config";
+import { createSession } from "./factories/session-factory";
+import { createUser } from "./factories/user-factory";
+import { User } from "@prisma/client";
+
+export async function cleanDb() {
+    await prisma.session.deleteMany({});
+    await prisma.hourControl.deleteMany({});
+    await prisma.bankHours.deleteMany({});
+    await prisma.user.deleteMany({});
+}
+
+export async function generateValidToken(user?: User) {
+    const incomingUser = user || (await createUser());
+    const token = jwt.sign({ userId: incomingUser.id }, process.env.JWT_SECRET);
+
+    await createSession(token);
+
+    return token;
+}
