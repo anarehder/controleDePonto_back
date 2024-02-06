c4669fd3c9da8f9b6c5459625a5bca9c0bd13e58
anarehder <anapaula.rehder@gmail.com>
2024-02-06 14:33:45 -0300

    feat: change password route

--- src/routers/users-router.ts
+++ src/routers/users-router.ts
@@ -1,12 +1,13 @@
-import { createUserController, getUsersController } from "../controllers";
+import { changePasswordController, createUserController, getUsersController } from "../controllers";
 import { authenticateToken, validateBody } from "../middlewares";
-import { createUserSchema } from "../schemas";
+import { createUserSchema, loginSchema } from "../schemas";
 import { Router } from "express";
 
 const usersRouter = Router();
 
 usersRouter.post("/", authenticateToken, validateBody(createUserSchema), createUserController);
 usersRouter.get("/", authenticateToken, getUsersController);
+usersRouter.post("/changepassword", authenticateToken, validateBody(loginSchema), changePasswordController);
 usersRouter.put("/", (_req, res) => res.send("PUT!"));
 usersRouter.delete("/", (_req, res) => res.send("DELETE!"));
 
