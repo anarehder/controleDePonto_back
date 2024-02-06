c91ac65688e44b9a3166c965871fce44f0db1da6
anarehder <anapaula.rehder@gmail.com>
2024-02-06 16:45:44 -0300

    feat: logOperations login

--- src/repositories/login-repository.ts
+++ src/repositories/login-repository.ts
@@ -2,17 +2,43 @@ import { prisma } from "../config";
 import { SessionInput } from "../protocols";
 
 export async function createSessionRepository (data: SessionInput) {
-
-    return prisma.session.create({
+    const session = await prisma.session.create({
         data,
     });
+
+    await prisma.logOperation.create({
+        data: {
+            employeeId: session.employeeId,
+            tableChanged: "session",
+            operation: "INSERT",
+            newValue: JSON.stringify(session),
+        }
+    });
+
+    return session;
 }
 
 
 export async function deleteSessionRepository (token: string) {
-    return prisma.session.delete({
+    const session = await prisma.session.findFirst({
+        where: {
+            token,
+        }
+    });
+    const deleted = await prisma.session.delete({
         where: {
             token,
         }
     })
+
+    await prisma.logOperation.create({
+        data: {
+            employeeId: deleted.employeeId,
+            tableChanged: "user",
+            operation: "UPDATE",
+            lastValue: JSON.stringify(session),
+        }
+    });
+
+    return deleted;
 }
